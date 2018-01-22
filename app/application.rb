141d00df44cc0abfaddc6fd36b8361cb4b715919
Parker Catalano <catalano.work@gmail.com>
2018-01-22 04:02:31 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,14 +1,16 @@
 require 'pry'
 class Application
-  resp = Rack::Response.new
 
-  time = Time.now
+  def call(env)
+    resp = Rack::Response.new
+    time = Time.now
 
-  if time.hour > 12
-    resp.write "Good Afternoon!"
-  else
-    resp.write "Good Morning!"
+    if time.hour > 12
+      resp.write "Good Afternoon!"
+    else
+      resp.write "Good Morning!"
+    end
+    
+    resp.finish
   end
-  
-  resp.finish
 end
